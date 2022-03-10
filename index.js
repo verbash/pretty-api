f21090e9d318d3b3318ee5733cf640ef98543243
Crystal Parker <vertfromage@gmail.com>
2022-03-10 19:19:26 -0400

    added some routes, wasn't able to test because couldn't get psql set up, also notes

--- index.js
+++ index.js
@@ -14,25 +14,26 @@ app.listen(5000, () => {
 //ROUTES
 
 //CREATE A PUNK - will need to update to add authentication
+// req.body - https://www.geeksforgeeks.org/express-js-req-body-property/ *Updated to accept multiple parameters* 
 app.post("/punk", async (req, res) => {
     try {
         console.log(req.body);
-        const { description } = req.body;
-        const newTodo = await pool.query(
+        const data = req.body;
+        const newPunk = await pool.query(
             "INSERT INTO punks (punk, rarity, cosplay, img, thumb, token_series_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
-        [description]
+        [data.punk, data.rarity, data.cosplay, data.img, data.thumb, data.token_series_id]
         );
 
-        res.json(newTodo.rows);
+        res.json(newPunk.rows);
     } catch (err) {
         console.error(err.message);
     }
 })
 
 //GET ALL PUNKS
-app.get("/todos", async (req, res) => {
+app.get("/punk", async (req, res) => {
     try {
-        const allPunks = await pool.query("SELECT * FROM todo");
+        const allPunks = await pool.query("SELECT * FROM punks");
         res.json(allPunks.rows);
     } catch (err) {
         console.error(err.message);
@@ -40,18 +41,39 @@ app.get("/todos", async (req, res) => {
 })
 
 //GET A PUNK - to-do
-// app.get("/punks/:id", async (req, res) => {
-//     try {
-//         console.log(req.params);
-//         const { id } = req.params;
-//         const todo = await pool.query("SELECT * FROM todo WHERE punk_id = $1", [id]);
-//         res.json(todo.rows);
-//     } catch (err) {
-//         console.error(err.message);
-//     }
-// })
+app.get("/punk/:id", async (req, res) => {
+    try {
+        console.log(req.params);
+        const { id } = req.params;
+        const apunk = await pool.query("SELECT * FROM punks WHERE punks_id = $1", [id]);
+        res.json(apunk.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 //UPDATE A PUNK
-
+app.put("/punk/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updatePunks = await pool.query("UPDATE punks SET description = $1 WHERE punks_id = $2", 
+        [description, id]);
+        res.json("Punk was updated");
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 //DELETE A PUNK
 
+app.delete("/punk/:id", async(req, res)=>{
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const deletePunk = await pool.query("DELETE FROM punks WHERE punks_id = $1", 
+        [id]);
+        res.json("Punk was deleted");
+    } catch (err) {
+        console.error(err.message);
+    }
+})
